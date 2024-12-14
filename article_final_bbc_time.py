05a762fff704a3cfcb9ff27998b407beb12398a6
Yuzhenn <c112156249@nkust.edu.tw>
2024-12-14 21:20:00 +0800

    Yuzhenn

---
+++ article_final_bbc_time.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import schedule
+import time
+import threading
+
+app = Flask(__name__)
+
+# 設定你的 Line Bot API 和 Webhook Handler
+line_bot_api = LineBotApi('+NcfxUIRlH9yCelBh0LCGQXhrMa2LUzNmtrNScDS+HWliUj6JWVDdkXjdlAP4qWMiMgQiieJQe1CkTjM9w/piTvz+Lbe+08mm/Yj1RBYWv/oiEAp8gKgCYtRmi8d41yQUFAcaU+hVA/36Vi+Tzh9ogdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('cc6b009b5871202a33b87f7201be3fe0')
+
+def get_bbc_news_links():
+    url = 'https://www.bbc.com/news'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    for a_tag in soup.find_all('a', href=True):
+        href = a_tag['href']
+        if href.startswith('/news/articles/'):
+            full_url = 'https://www.bbc.com' + href
+            return full_url
+
+def send_daily_article():
+    news_link = get_bbc_news_links()
+    if news_link:
+        reply = news_link
+    else:
+        reply = "No articles found."
+    line_bot_api.push_message('U2b9073cc63a1406db2c434341ad29f21', TextSendMessage(text=reply))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 獲取請求標頭中的簽名
+    signature = request.headers['X-Line-Signature']
+
+    # 獲取請求正文
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # 驗證簽名
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+def schedule_daily_message():
+    schedule.every().day.at("17:47").do(send_daily_article)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    # 啟動排程執行緒
+    threading.Thread(target=schedule_daily_message).start()
+    # 啟動 Flask 伺服器
+    app.run()
