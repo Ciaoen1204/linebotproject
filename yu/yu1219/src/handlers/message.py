2747c84c01f3a1903df0ae315da86182f19e1d19
yu2129 <C112156207@ckust.edu.tw>
2024-12-19 11:32:04 +0800

    yuu

--- yu/yu1219/src/handlers/message.py
+++
@@ -1,7 +0,0 @@
-class MessageHandler:
-    def __init__(self, generative_ai):
-        self.generative_ai = generative_ai
-
-    def handle_message(self, message):
-        response_text = self.generative_ai.generate_response(message)
-        return response_text
